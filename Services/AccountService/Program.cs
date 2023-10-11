2ae7be009cd6f4b7dbc6e21eb37085f1d2d086c1
Marius <marius.badrajan@mail.ru>
2023-10-12 01:56:52 +0300

    Add proto file and account service

--- Services/AccountService/Program.cs
+++ Services/AccountService/Program.cs
@@ -1,5 +1,3 @@
-using AccountService.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
@@ -11,7 +9,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<AccountService.Services.AccountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
--- Services/AccountService/Services/AccountService.cs
+++ Services/AccountService/Services/AccountService.cs
@@ -1,6 +1,107 @@
+using AccountService.Proto;
+using AccountService.Repositories;
+using Grpc.Core;
+using AccountServices = AccountService.Proto.AccountService;
+
 namespace AccountService.Services;
 
-public class AccountService
+public class AccountService : AccountServices.AccountServiceBase
 {
+    private readonly IAccountRepository _accountRepository;
+
+    public AccountService(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
     
+    public override async Task<UserAccounts> GetUserAccounts(Empty request, ServerCallContext context)
+    {
+        var dbUserAccounts = await _accountRepository.GetAllUserAccountsAsync();
+        
+        var userAccounts = new UserAccounts();
+        foreach (var userAccount in dbUserAccounts)
+        {
+            userAccounts.Accounts.Add(new UserAccount
+            {
+                Id = userAccount.Id.ToString("D"),
+                UserId = userAccount.UserId.ToString("D"),
+                Balance = userAccount.Balance
+            });
+        }
+
+        return userAccounts;
+    }
+
+    public override async Task<UserAccounts> GetAccountsByUserId(GetAccountsByUserIdRequest request,
+        ServerCallContext context)
+    {
+        var dbUserAccounts = await _accountRepository.GetAllAccountsByUserIdAsync(new Guid(request.UserId));
+        
+        var userAccounts = new UserAccounts();
+        foreach (var userAccount in dbUserAccounts)
+        {
+            userAccounts.Accounts.Add(new UserAccount
+            {
+                Id = userAccount.Id.ToString("D"),
+                UserId = userAccount.UserId.ToString("D"),
+                Balance = userAccount.Balance
+            });
+        }
+
+        return userAccounts;
+    }
+
+    public override async Task<UserAccount> GetUserAccountById(GetUserAccountByIdRequest request,
+        ServerCallContext context)
+    {
+        var dbUserAccount = await _accountRepository.GetUserAccountByIdAsync(new Guid(request.Id));
+
+        var userAccount = new UserAccount()
+        {
+            Id = dbUserAccount.Id.ToString("D"),
+            UserId = dbUserAccount.UserId.ToString("D"),
+            Balance = dbUserAccount.Balance
+        };
+
+        return userAccount;
+    }
+
+    public override async Task<UserAccount> AddUserAccount(AddUserAccountRequest request,
+        ServerCallContext context)
+    {
+        var dbUserAccount = new Entities.UserAccount()
+        {
+            Id = new Guid(request.Account.Id),
+            UserId = new Guid(request.Account.UserId),
+            Balance = request.Account.Balance
+        };
+
+        var userAccount = await _accountRepository.AddUserAccountAsync(dbUserAccount);
+
+        return request.Account;
+    }
+
+    public override async Task<UserAccount> UpdateOffer(UpdateUserAccountRequest request,
+        ServerCallContext context)
+    {
+        var dbUserAccount = new Entities.UserAccount()
+        {
+            Id = new Guid(request.Account.Id),
+            UserId = new Guid(request.Account.UserId),
+            Balance = request.Account.Balance
+        };
+
+        var userAccount = await _accountRepository.UpdateUserAccountAsync(dbUserAccount);
+
+        return request.Account;
+    }
+    
+    public override async Task<Empty> DeleteOffer(DeleteUserAccountRequest request,
+        ServerCallContext context)
+    {
+        
+        await _accountRepository.DeleteUserAccountByIdAsync(new Guid(request.Id));
+
+        return new Empty();
+    }
 }

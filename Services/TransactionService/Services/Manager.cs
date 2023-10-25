232b330a531f98d2ca4f7e0d991dab4e836e4599
Marius Badrajan <marius.badrajan@mail.ru>
2023-10-26 01:26:45 +0300

    Add services health check and create docker compose file

---
+++ Services/TransactionService/Services/Manager.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Microsoft.Data.SqlClient;
+using TransactionService.Proto;
+using ManagerService = TransactionService.Proto.ManagerService;
+
+
+namespace AccountService.Services
+{
+    public class Manager : ManagerService.ManagerServiceBase
+    {
+        public override async Task<Response> GetHealthStatus(Request request, ServerCallContext context)
+        {
+            using var connection = new SqlConnection("Server=MDMOB40630; Database=AccountsDb; Integrated Security=True; TrustServerCertificate=True;");
+
+            await connection.OpenAsync();
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                return new Response { HealthStatus = "Unhealthy" };
+            }
+
+            return new Response { HealthStatus = "Healthy" };
+        }
+    }
+}
